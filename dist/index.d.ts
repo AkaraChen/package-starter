853c886d7b325651d65a43178d50a8d3be136d8f
AkaraChen <85140972+AkaraChen@users.noreply.github.com>
2022-09-13 08:22:40 +0800

    ✨ feat: build before commit

---
+++ dist/index.d.ts
@@ -0,0 +1,3 @@
+export declare const greet: (name?: string) => void;
+declare const _default: number;
+export default _default;
---
+++ dist/index.js
@@ -0,0 +1,2 @@
+var o=e=>++e;var n=(e="World")=>console.log(`Hello, ${e}!`),l=o(0);export{l as default,n as greet};
+//# sourceMappingURL=index.js.map
