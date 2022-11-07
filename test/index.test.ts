6f3dfffc9d6a79a51bf6477f76c0a025ef5bde25
AkaraChen <85140972+AkaraChen@users.noreply.github.com>
2022-11-07 11:05:22 +0800

    🐳 chore: remove unused

---
+++ test/index.test.ts
@@ -0,0 +1,8 @@
+// @vitest-environment node
+import {test, expect, describe} from 'vitest';
+
+describe('index module', () => {
+    test('1 equal 1', () => {
+        expect(1).toBe(1);
+    });
+});
