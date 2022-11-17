7626badbeeb404515ad6b31fb344990c5c0dd10b
AkaraChen <85140972+AkaraChen@users.noreply.github.com>
2022-11-17 22:33:18 +0800

    🔧 build: new eslint config

--- vitest.config.ts
+++ vitest.config.ts
@@ -2,6 +2,6 @@ import {defineConfig} from 'vitest/config';
 
 export default defineConfig({
     test: {
-        include: ['./test/**/*.test.{js,mjs,cjs,ts,mts,cts,jsx,tsx}'],
-    },
+        include: ['./test/**/*.test.{js,mjs,cjs,ts,mts,cts,jsx,tsx}']
+    }
 });
